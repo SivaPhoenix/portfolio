622567aefeaabd92ef089e951943cee9284b414c
Siva Sriram <113771235+SivaPhoenix@users.noreply.github.com>
2024-01-20 23:50:22 +0530

    Update Home2.js

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -49,7 +49,7 @@ function Home2() {
               </i>
               &nbsp; like
               <i>
-                <b className="purple"> React.js and Next.js</b>
+                <b className="purple"> React.js</b>
               </i>
             </p>
           </Col>
