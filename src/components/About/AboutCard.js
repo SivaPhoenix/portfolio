be2e3af329f9a5b6243c88f9644c1dd4deb0489f
Siva Sriram <113771235+SivaPhoenix@users.noreply.github.com>
2024-09-02 15:21:08 +0530

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,7 +8,7 @@ function AboutCard() {
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
             Hi Everyone, I am <span className="purple">Siva Sriram </span>
-            from <span className="purple"> Kallakurichi, Tamil Nadu, India.</span>
+            from <span className="purple"> Kallakurichi,Tamil Nadu,India.</span>
             <br /> I am a final year student pursuing an B.Tech (Information Technology) in Sri Ramakrishna Engineering College,Coimbaore.
             <br />
             <br/>
