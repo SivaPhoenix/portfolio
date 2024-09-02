4b8d8f270ab56963fff5a8e89b9f58847dae479f
Siva Sriram <113771235+SivaPhoenix@users.noreply.github.com>
2024-09-02 12:25:16 +0530

    Update year

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,14 +8,14 @@ function AboutCard() {
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
             Hi Everyone, I am <span className="purple">Siva Sriram </span>
-            from <span className="purple"> Kallakurichi,Tamil Nadu,India.</span>
-            <br /> I am a third year student pursuing an B.Tech (Information Technology) in Sri Ramakrishna Engineering College,Coimbaore.
+            from <span className="purple"> Kallakurichi, Tamil Nadu, India.</span>
+            <br /> I am a final year student pursuing an B.Tech (Information Technology) in Sri Ramakrishna Engineering College,Coimbaore.
             <br />
             <br/>
           </p>
           <br/>
           <p style={{ color: "rgb(155 126 172)" }}>
-            "I trained $years to run 9 seconds and people give up when they don't see results in 2 month"{" "}
+            "I trained 4 years to run 9 seconds and people give up when they don't see results in 2 month"{" "}
           </p>
           <footer className="blockquote-footer">Usain Bolt</footer>
         </blockquote>
